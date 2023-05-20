e429cf472c23d6564c6838107f1b0ad344e834cf
Breno Goddard <brenogoddard@hotmail.com>
2023-05-20 16:46:55 -0300

    primeiro commit

---
+++ Modules/calc.js
@@ -0,0 +1,3 @@
+export function calculo (valor1,valor2) {
+    return valor1 * valor2;
+}
---
+++ Modules/script.js
@@ -0,0 +1,7 @@
+import { alerta, textoAlerta } from './module.js'
+
+import { calculo } from './calc.js'
+
+console.log(calculo(2,5))
+
+alerta(textoAlerta);
